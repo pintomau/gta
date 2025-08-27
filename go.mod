0c7dad3607736597625362d68bcbb14bc336cd27
Mauro Pinto <mauro.pinto@diconium.com>
2025-08-27 09:33:07 +0100

    feat: change namespace

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/digitalocean/gta
+module github.com/pintomau/gta
 
 go 1.24
 
--- gtaintegration/gtaintegration_test.go
+++ gtaintegration/gtaintegration_test.go
@@ -17,7 +17,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/digitalocean/gta"
+	"github.com/pintomau/gta"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/pkg/errors"
